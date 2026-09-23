75e90d2847e08917e94e68de09f14c1db4bb2fc6
driannn04 <saputraandrian423@gmail.com>
2026-09-23 17:00:03 +0700

    feat: connect all routes - auth + quiz fully integrated

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,27 +1,37 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import QuizPage from "./pages/QuizPage";
+import ResultPage from "./pages/ResultPage";
+import LeaderboardPage from "./pages/LeaderboardPage";
 
-// TODO: Orang 2 (feature/quiz) akan tambahkan import berikut setelah merge:
-// import HomePage from "./pages/HomePage";
-// import QuizPage from "./pages/QuizPage";
-// import ResultPage from "./pages/ResultPage";
-// import LeaderboardPage from "./pages/LeaderboardPage";
+function AppRoutes() {
+  const { currentUser } = useAuth();
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={!currentUser ? <LoginPage /> : <Navigate to="/" replace />} />
+        <Route path="/register" element={!currentUser ? <RegisterPage /> : <Navigate to="/" replace />} />
+        <Route path="/" element={<ProtectedRoute><HomePage currentUser={currentUser} /></ProtectedRoute>} />
+        <Route path="/quiz/:category" element={<ProtectedRoute><QuizPage currentUser={currentUser} /></ProtectedRoute>} />
+        <Route path="/result" element={<ProtectedRoute><ResultPage /></ProtectedRoute>} />
+        <Route path="/leaderboard" element={<ProtectedRoute><LeaderboardPage currentUser={currentUser} /></ProtectedRoute>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* Routes quiz akan ditambahkan Orang 2 via feature/quiz */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
   );
